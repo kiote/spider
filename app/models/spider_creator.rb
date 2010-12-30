8c43a667e860bafb0e393e905b182adc8a643a86
Krivich Ekaterina <krivich.ekaterina@gmail.com>
2010-12-30 18:43:01 +0300

    fix

--- app/models/spider_creator.rb
+++ app/models/spider_creator.rb
@@ -6,21 +6,29 @@ class SpiderCreator
   def generate
     xml = XmlGenerator.new
     result = xml.to_s
+
     @section.each do |url|
       spider = SpiderParser.new(url)
-      result += xml.add_tournament(spider.tournament)
 
-      #выбираем команды, события и ставки
-      competitions = spider.team
-      events = spider.event
-      factors = spider.factor
+      tournament = spider.tournament
 
-      competitions.each_with_index do |item, index|
-        result += xml.add_competition(item[0], item[1])
-        events.each_with_index do |event, event_index|
-          result += xml.add_event(event, factors[index][event_index])
+      if not tournament.empty?
+        result += xml.add_tournament(spider.tournament)
+
+        #выбираем команды, события и ставки
+        competitions = spider.team
+        events = spider.event
+        factors = spider.factor
+
+        competitions.each_with_index do |item, index|
+          result += xml.add_competition(item[0], item[1])
+          events.each_with_index do |event, event_index|
+            result += xml.add_event(event, factors[index][event_index])
+          end
+          result += xml.close_competition_tag
         end
-        result += xml.close_competition_tag
+
+        result += xml.close_tournament_tag
       end
     end
     
