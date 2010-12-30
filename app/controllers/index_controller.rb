3764cda1f6166dfd120f20679a1a2640b0f7e6e1
Krivich Ekaterina <krivich.ekaterina@gmail.com>
2010-12-30 18:47:19 +0300

    добавлены разделы

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,7 +3,19 @@ require 'open-uri'
 
 class IndexController < ApplicationController
   def index
-    links = %w(betoffer/1/1211 betoffer/1/1103 betoffer/1/922)
+    links = %w(
+      betoffer/1/1211
+      betoffer/1/1103
+      betoffer/1/922
+      betoffer/1/1613 
+      betoffer/1/1645
+      betoffer/1/916
+      betoffer/1/3554
+      betoffer/1/4636
+      betoffer/1/2580
+      betoffer/1/5174
+      betoffer/1/5194
+    )
     creator = SpiderCreator.new(links)
     @content = creator.generate
 
