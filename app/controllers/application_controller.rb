8f71056f15f007413a81605ed0fb20fb04c08a14
Kiote <krivich.ekaterina@gmail.com>
2011-01-02 20:40:10 +0300

    формат по умолчанию - xml

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 $KCODE = 'u'
 require 'jcode' 
 class ApplicationController < ActionController::Base
+  before_filter :set_default_response_format
+
+  protected
+
+    def set_default_response_format
+      request.format = 'xml'.to_sym if params[:format].nil?
+    end
   protect_from_forgery
 end
