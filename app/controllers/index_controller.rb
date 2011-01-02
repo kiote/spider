57cd2c233f7cfc0d76f3267f59c3038ae548782b
Kiote <krivich.ekaterina@gmail.com>
2011-01-02 22:20:17 +0300

    yaml added

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,27 +1,40 @@
 require 'nokogiri'
 require 'open-uri'
+require 'yaml'
 
 class IndexController < ApplicationController
-  def index
-    links = %w(
-      betoffer/1/1211
-      betoffer/1/1103
-      betoffer/1/922
-      betoffer/1/1613 
-      betoffer/1/1645
-      betoffer/1/916
-      betoffer/1/3554
-      betoffer/1/4636
-      betoffer/1/2580
-      betoffer/1/5174
-      betoffer/1/5194
-    )
-    creator = SpiderCreator.new(links)
-    @content = creator.generate
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @content }
+  before_filter :configure_app
+
+  def index
+    params['name']
+#    links = %w(
+#      betoffer/1/1211
+#      betoffer/1/1103
+#      betoffer/1/922
+#      betoffer/1/1613
+#      betoffer/1/1645
+#      betoffer/1/916
+#      betoffer/1/3554
+#      betoffer/1/4636
+#      betoffer/1/2580
+#      betoffer/1/5174
+#      betoffer/1/5194
+#    )
+#    creator = SpiderCreator.new(links)
+#    @content = creator.generate
+#
+#    respond_to do |format|
+#      format.html
+#      format.xml  { render :xml => @content }
+#    end
+    services = @config['services'].each |service|
+      print service
     end
+    @content = @config['services']
+  end
+
+  def configure_app
+    @config = YAML::load(File.open("#{Rails.root}/config/spider.yml"))
   end
 end
